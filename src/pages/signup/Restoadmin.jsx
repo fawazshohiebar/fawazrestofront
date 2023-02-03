76826c4500d570951c6100c1e0f1d3babb24cbe7
fawazshohiebar <fawazsho.132@gmail.com>
2023-02-03 03:49:57 +0200

    second commit for tonight 349

--- src/pages/signup/Restoadmin.jsx
+++ src/pages/signup/Restoadmin.jsx
@@ -18,6 +18,62 @@ function Restoadmin() {
     const [category_id, setcategory_id] = useState(null)
     const [itemgetter, setitemgetter] = useState(null);
     const [Nresto_name, setNresto_name] = useState(null);
+    const [catname, setcatname] = useState(null)
+    const [itemname, setitemname] = useState(null)
+    const [itemdescription, setitemdescription] = useState(null)
+    const [itemprice, setitemprice] = useState(0);
+    const [itemtags, setitemtags] = useState([])
+
+
+
+
+    const changerestoname = async () => {
+        const data = { _id: resto_id, restaurant_name: Nresto_name };
+        await axios.put(`http://localhost:3000/restaurant/name`, data)
+            .then(res => {
+                console.log("Successful update: ", res.data);
+            })
+            .catch(err => {
+                console.error("Update failed: ", err);
+            });
+    }
+
+
+
+
+
+
+    // this is for editing the image keepit as it is    
+    const [file, setFile] = useState(null);
+    const handleFileUpload = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("resto_logo", file);
+        formData.append("_id", resto_id);
+        axios
+            .put("http://localhost:3000/restaurant/logo", formData)
+            .then((res) => {
+                console.log("everything is done ")
+            })
+            .catch((err) => {
+                console.log(err.response.data.error);
+            });
+    };
+
+
+
+
+
+
+
+
+
+
+
 
 
     const getrestoname = () => {
@@ -92,11 +148,70 @@ function Restoadmin() {
     }
 
 
+    const addnewcat = async () => {
+        const data = { resto_id: resto_id, category_name: catname };
+        await axios.post(`http://localhost:3000/categories/post`, data)
+            .then(res => {
+                console.log("Successful update: ", res.data);
+            })
+            .catch(err => {
+                console.error("Update failed: ", err);
+            });
+    }
+
+
+
+
+
+    const addnewitem = async () => {
+        const data = { cat_id: category_id, item_name: itemname, item_description: itemdescription, item_price: itemprice, item_tags: itemtags };
+        await axios.post(`http://localhost:3000/item/post`, data)
+            .then(res => {
+                console.log("Successful update: ", res.data);
+            })
+            .catch(err => {
+                console.error("Update failed: ", err);
+            });
+    }
+
+
+
+
+
 
     const resto_namechanger = (event) => {
         setNresto_name(event.target.value);
         console.log(event.target.value)
     };
+
+
+    const handlecatname = (event) => {
+        setcatname(event.target.value);
+        console.log(event.target.value)
+    };
+
+
+    const handleitemname = (event) => {
+        setitemname(event.target.value);
+        console.log(event.target.value)
+    };
+    const handleitemdescription = (event) => {
+        setitemdescription(event.target.value);
+        console.log(event.target.value)
+    };
+    const handleitemprice = (event) => {
+        setitemprice(event.target.value);
+        console.log(event.target.value)
+    };
+
+
+    const handleitemtags = (event) => {
+        setitemtags([...itemtags, event.target.value]);
+    }
+
+
+
+
 
 
     const openresto_name = () => {
@@ -109,15 +224,31 @@ function Restoadmin() {
 
 
 
+    const opencatname = () => {
+        document.getElementById("categoryform").style.display = "block";
+    }
+    const closecatname = () => {
+        document.getElementById("categoryform").style.display = "none";
+    }
+    const openadditems = () => {
+        document.getElementById("additems").style.display = "block";
+    }
+    const closeadditems = () => {
+        document.getElementById("additems").style.display = "none";
+    }
+
+
+
+
+
+
+
     useEffect(() => {
         getrestoname()
         getRestoLogo()
         getcategories()
 
-    }, [resto_id, category_id])
-
-
-
+    }, [resto_id, resto_name, category_id, Nresto_name])
 
 
 
@@ -132,9 +263,19 @@ function Restoadmin() {
             <div className='container'>
                 <img src={`http://localhost:3000/${resto_logo}`} alt='logo for rest'></img>
                 <label id='imgbtn' htmlFor="filePicker" >
+
+
                     <img id='fileimg' src={edit} alt=''></img>
                 </label>
-                <input id="filePicker" type={"file"}></input>
+                <br />
+                <br />
+                <br />
+                <form onSubmit={handleSubmit}>
+                    <input type="file" onChange={handleFileUpload} />
+                    <button type="submit">Submit</button>
+
+                </form>
+
 
             </div>
             <div className='container2'>
@@ -146,10 +287,10 @@ function Restoadmin() {
                 <div className="form-popup right" id="resto_name">
 
                     <h1>edit the name of the restaurant  </h1>
-                    <label className="textform" htmlFor="email">Restaurant name </label><br />
+                    <label className="textform" >Restaurant name </label><br />
                     <input type="text" value={Nresto_name} onChange={resto_namechanger} /><br />
 
-                    <button className="btn" >submit</button><br /><br />
+                    <button className="btn" onClick={changerestoname} >change</button><br /><br />
                     <button onClick={closeresto_name} className="btn cancel">close me </button>
 
                 </div>
@@ -162,6 +303,7 @@ function Restoadmin() {
 
 
             </div>
+
             <nav>
                 <div className='wrapper'>
                     <div className='navbtn'></div>
@@ -172,10 +314,55 @@ function Restoadmin() {
                         ))
 
                     }
-                    <button>+</button>
+                    <button onClick={opencatname}>+</button>
+
+
+
+
+
+                    <div className="form-popup left" id="categoryform">
+
+                        <h1>ADD NEW category </h1>
+                        <label className="textform" htmlFor="email">categoryname</label><br />
+                        <input type="text" value={catname} onChange={handlecatname} />
+                        <br />
+
+                        <button className="btn " onClick={addnewcat}>submit</button><br /><br />
+                        <button onClick={closecatname} className="btn cancel">close me </button>
+
+                    </div>
 
                 </div>
             </nav>
+
+
+            <button onClick={openadditems}>additems</button>
+
+
+            <div className="form-popup left" id="additems">
+
+                <h1>ADD NEW item </h1>
+                <label className="textform" >item_name</label><br />
+                <input type="text" value={itemname} onChange={handleitemname} />
+                <br />
+                <label className="textform" >item_description</label><br />
+                <input type="text" value={itemdescription} onChange={handleitemdescription} />
+                <br />
+                <label className="textform" >item_price</label><br />
+                <input type="number" value={itemprice} onChange={handleitemprice} />
+                <br />
+                <label className="textform" >item_tags</label><br />
+                <input type="text" value={itemtags} onChange={handleitemtags} />
+                <br />
+
+
+                <button className="btn " onClick={addnewitem}>submit</button><br /><br />
+                <button onClick={closeadditems} className="btn cancel">close me </button>
+
+            </div>
+
+
+
 
 
 
