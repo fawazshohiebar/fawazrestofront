9274ea98c225d0d7a6912480ab393137ef595522
shant-k <shantk650@gmail.com>
2023-02-09 18:37:28 +0200

    added  signup functionality

--- src/pages/pages/signup.js
+++ src/pages/pages/signup.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./signin.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  
-const role ="admin"
-  async function loginUser(event) {
+  const navigate = useNavigate();
+
+  const role = "admin";
+  async function createUser(event) {
     event.preventDefault();
     const x = await fetch("http://localhost:3000/users/new", {
       method: "POST",
@@ -22,13 +24,34 @@ const role ="admin"
     });
 
     const response = await x.json();
-    console.log(response);
+    console.log(response)
+    const createResto = await fetch("http://localhost:3000/restaurants/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id:response._id,
+        restaurant_name:"default name",
+        is_disabled: false
+      }),
+     
+    });
+    const created = await createResto.json();
+    console.log(created)
+    localStorage.setItem("id", response._id);
+    localStorage.setItem("role", response.role);
+    if(!created){
+      navigate("/users/new", { replace: true });
+    }else if (created){
+      navigate("/restaurant", { replace: true });
+    }
   }
   return (
     <>
       <div className="container-all">
         <h1 className="title">Sign Up</h1>
-        <form className="form-container1" onSubmit={loginUser}>
+        <form className="form-container1" onSubmit={createUser}>
           <label className="user-label">Username</label>
 
           <input
@@ -50,7 +73,6 @@ const role ="admin"
             id="password"
           />
 
-          
           <input className="submit-btn" type="submit" value="Register" />
         </form>
       </div>
